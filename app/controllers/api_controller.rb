6e428762fcc6f8b4136348f29bbf9ab9af31eff0
pedro823 <grey.pedrinho@gmail.com>
2018-06-09 23:41:11 -0300

    Adds POST to study room

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,4 +1,6 @@
 class ApiController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def all_institutions
     all = {
       :institutions => []
@@ -31,9 +33,44 @@ class ApiController < ApplicationController
     render json: rooms.to_json
   end
 
+  def set_study_room
+    body = JSON.parse(request.body.read)
+    puts body
+    if (free = body['free']) == nil or (id = body['id']) == nil
+      bad_request('missing arguments: be sure free and id are in the body') and return
+    end
+    if free != true and free != false
+      bad_request('free must be a boolean') and return
+    end
+    unless StudyRoom.exists?(id)
+      not_found("study room with id #{id} was not found") and return
+    end
+    study_room = StudyRoom.find(id)
+    study_room.is_free = free
+    if study_room.save
+      result = {
+        success: true,
+        new_state: free
+      }
+      render json: result.to_json and return
+    else
+      internal_error("Could not save study_room") and return
+    end
+  end
+
+  def plan_study
+  end
 
 
   private
+
+  def internal_error(text="")
+    message = {
+      :error => "Internal Error",
+      :description => text
+    }
+    render json: message.to_json, status: 500
+  end
 
   def not_found(text="")
     message = {
@@ -41,5 +78,13 @@ class ApiController < ApplicationController
       :description => text
     }
     render json: message.to_json, status: 404
+  end
+
+  def bad_request(text="")
+    message = {
+      :error => "Bad Request",
+      :description => text
+    }
+    render json: message.to_json, status: 400
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get '/', to: 'search#index'
   get '/institutions', to: 'api#all_institutions'
   get '/institutions/:id', to: 'api#institution'
+  get '/plan-study/:id', to: 'api#plan_study'
+  post '/study', to: 'api#set_study_room'
 end
